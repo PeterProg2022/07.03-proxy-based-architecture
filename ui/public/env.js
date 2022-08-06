cd4f56d724198893fb24123459fd6929de3efcac
Peter Prog <peter.prog@yahoo.com>
2022-08-07 07:13:25 +0800

    ui: env

---
+++ ui/public/env.js
@@ -0,0 +1,12 @@
+// file nur in der dokumentation (978-1-4842-4390-9--Pro_MERN_Stack-Full_Stack_Web_App_Development)
+// nicht im dl-code
+
+window.ENV = {
+// 	 local
+// 	UI_SERVER_PORT:8001,
+// 	UI_API_ENDPOINT:'http://localhost:4001/graphql',
+
+// 	remote
+// 	UI_SERVER_PORT:8000,	in vercel nicht zu setzten möglich
+	UI_API_ENDPOINT:'https://07-03-api.vercel.app/graphql',
+}
